d279e39f64af2b4e2cc1a4dee20360a9ff264cf7
野声 <lijiacheng.ljc@antgroup.com>
2024-08-23 13:08:05 +0800

    Initialize language parser

---
+++ src/language-facts/base.ts
@@ -0,0 +1,46 @@
+import Parser from 'web-tree-sitter';
+
+import { SupportedTreeSitterLanguages } from './types';
+import { IRange } from '../types';
+
+interface IBaseCodeBlockInfo {
+  infoCategory: string;
+  type: string;
+  range: IRange;
+}
+
+export interface IFunctionBlockInfo extends IBaseCodeBlockInfo {
+  infoCategory: 'function';
+
+  name: string;
+  signatures: string[];
+}
+
+export interface IOtherBlockInfo extends IBaseCodeBlockInfo {
+  infoCategory: 'other';
+}
+
+interface IBlockCommentStyle {
+  start: string;
+  end: string;
+  linePrefix: string;
+}
+
+export type ICodeBlockInfo = IFunctionBlockInfo | IOtherBlockInfo;
+
+export abstract class AbstractLanguageFacts {
+  abstract name: SupportedTreeSitterLanguages;
+
+  abstract listCommentStyle: string;
+  abstract blockCommentStyle: IBlockCommentStyle;
+
+  abstract provideFunctionInfo?(
+    node: Parser.SyntaxNode,
+  ): IFunctionBlockInfo | null;
+  abstract provideCodeBlocks(): Set<string>;
+  abstract isCodeBlock(type: string): boolean;
+  abstract isFunctionCodeBlocks?(type: string): boolean;
+}
+
+export type AbstractLanguageFactsDerived = (new () => AbstractLanguageFacts) &
+  typeof AbstractLanguageFacts;
---
+++ src/language-facts/types.ts
@@ -0,0 +1,27 @@
+import { invert } from '../utils';
+
+const map = {
+  typescript: 'typescript',
+  typescriptreact: 'tsx',
+  javascript: 'javascript',
+  javascriptreact: 'jsx',
+  rust: 'rust',
+  python: 'python',
+  java: 'java',
+  go: 'go',
+} as const;
+
+type ValueOf<T> = T[keyof T];
+
+export type SupportedLanguages = keyof typeof map;
+export type SupportedTreeSitterLanguages = ValueOf<typeof map>;
+
+export const parserNameMap: Record<
+  SupportedLanguages,
+  SupportedTreeSitterLanguages
+> = map;
+
+export const parserIdentifierMap = invert(parserNameMap) as Record<
+  SupportedTreeSitterLanguages,
+  SupportedLanguages
+>;
